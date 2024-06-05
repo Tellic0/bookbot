3653ddead56ab975a66faae12a9b5e5caadcf96e
Tellic0 <tellico.lunngrevink@gmail.com>
2024-06-06 00:14:54 +0200

    A: added read_contents function to main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello world")
+def read_contents(name):
+    with open(f"books/{name}") as f:
+        file_contents = f.read()
+    print(f"{file_contents}")
+
+
+def main():
+    read_contents("frankenstein.txt")
+
+
+main()
