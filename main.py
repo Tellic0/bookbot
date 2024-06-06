6efdf8ae52879d8b8a0fb04911a01fea8f5d144b
Tellic0 <tellico.lunngrevink@gmail.com>
2024-06-06 18:24:25 +0200

    D: add count_characters

--- main.py
+++ main.py
@@ -7,12 +7,35 @@ def count_words(file_contents):
     print(f"{len(words)}")
 
 
+def sort_on(dict):
+    return dict["num"]
+
+
+def count_characters(file_contents):
+    lower_string = file_contents.lower()
+    chars_dict = {}
+
+    for char in lower_string:
+        if char in chars_dict and char.isalpha():
+            chars_dict[char] = chars_dict[char] + 1
+        elif char.isalpha():
+            chars_dict[char] = 1
+
+    dict_list = []
+
+    for name in chars_dict:
+        dict_list.append({"char": name, "num": chars_dict[name]})
+
+    dict_list.sort(reverse=True, key=sort_on)
+    print(dict_list)
+
+
 def main():
     name = "frankenstein.txt"
     with open(f"books/{name}") as f:
         file_contents = f.read()
-    read_contents(file_contents)
     count_words(file_contents)
+    count_characters(file_contents)
 
 
 main()
